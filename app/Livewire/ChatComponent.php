48bb92548f3bd3407af14896cb41e37579632a76
khobabe <khobabe007@gmail.com>
2025-03-04 14:50:01 +0530

    it is not working , as there are some mistakes and typo. so i am gonna commit this as it is and then later on with altering all those changes. so that later in the future i can see what was the actual problem , where was i wrong? and learn from it

--- app/Livewire/ChatComponent.php
+++ app/Livewire/ChatComponent.php
@@ -2,9 +2,11 @@
 
 namespace App\Livewire;
 
+use App\Events\messageSendEvent;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ChatComponent extends Component
@@ -20,21 +22,22 @@ class ChatComponent extends Component
         return view('livewire.chat-component');
     }
 
-    public function mount($user_id){
+    public function mount($user_id)
+    {
 
         $this->sender_id = Auth::id();
         // $this->sender_id = auth()->user()->id();
         $this->reciever_id = $user_id;
 
-        $messages = Message::where(function($query){
-            $query->where('sender_id',$this->sender_id)->where('reciever_id',$this->reciever_id);
-        })->orWhere(function($query){
+        $messages = Message::where(function ($query) {
+            $query->where('sender_id', $this->sender_id)->where('reciever_id', $this->reciever_id);
+        })->orWhere(function ($query) {
             $query->where('sender_id', $this->reciever_id)->where('reciever_id', $this->sender_id);
         })->with('sender:id,name', 'reciever:id,name')->get();
 
         // dd($messages->toArray());
 
-        foreach($messages as $message){
+        foreach ($messages as $message) {
             $this->appendChatMessage($message);
         }
 
@@ -43,7 +46,8 @@ class ChatComponent extends Component
         $this->user = User::whereId($user_id)->first();
     }
 
-    public function sendMessage(){
+    public function sendMessage()
+    {
         // dd($this->message);
         $chatMessage = new Message();
 
@@ -51,16 +55,29 @@ class ChatComponent extends Component
         $chatMessage->reciever_id = $this->reciever_id;
         $chatMessage->message = $this->message;
         $chatMessage->save();
-        
+
+        $this->appendChatMessage($chatMessage);
+        broadcast(new messageSendEvent($chatMessage))->toOthers();
+
         $this->message = '';
     }
 
-    public function appendChatMessage($message){
+    #[On('echo-private:chat-channel.{sender_id},MessageSendEvent')]
+    public function listenForMessage($event)
+    {
+        $chatMessage = Message::whereId($event['message']['id'])
+            ->with('sender:id,name', 'reciever:id,name')->get()
+            ->first();
+        $this->appendChatMessage($chatMessage);
+    }
+
+    public function appendChatMessage($message)
+    {
         $this->messages[] = [
             'id' => $message->id,
             'message' => $message->message,
             'sender' => $message->sender->name,
-            'reciever' => $message->reciever->name,
+            'reciever' => $message->reciever->name
         ];
     }
 }
